d5256308c712e58bf8688aaf0c74813f2d739097
cristian Alvarez perez <calvarez@iota19.aulas.gsyc.es>
2014-11-17 16:47:56 +0100

    Servidor terminado sin pasar el pep8

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import SocketServer
 import sys
+import os
+import os.path
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -20,20 +22,40 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             if not line:
                 break
             print "El cliente nos manda " + line
+            Metodos = ["INVITE", "ACK", "BYE"]
             Metodo = line.split(" ")[0]
-            if Metodo == "INVITE":
-                Answer = "SIP/2.0 100 Trying\r\n\r\n"
-                Answer += "SIP/2.0 180 Ring\r\n\r\n"
-                Answer += "SIP/2.0 200 OK\r\n\r\n"
+            Mensaje = line.split(" ")
+            if len(Mensaje) == 3:
+                if Metodo in Metodos:
+                    if Metodo == "INVITE":
+                        Answer = "SIP/2.0 100 Trying\r\n\r\n"
+                        Answer += "SIP/2.0 180 Ring\r\n\r\n"
+                        Answer += "SIP/2.0 200 OK\r\n\r\n"
+                        self.wfile.write(Answer)
+                    elif Metodo == "ACK":
+                        fichero_audio = sys.argv[3]
+                        aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < "
+                        aEjecutar += fichero_audio
+                        print "Vamos a ejecutar", aEjecutar
+                        os.system("chmod 755 mp32rtp")
+                        os.system(aEjecutar)
+                    elif Metodo == "BYE":
+                        Answer = "SIP/2.0 200 OK\r\n\r\n"
+                        self.wfile.write(Answer)
+                else:
+                    Answer = "SIP/2.0 405 Method Not Allowed\r\n\r\n"
+                    self.wfile.write(Answer)
+            else:
+                Answer = "SIP/2.0 400 Bad Request\r\n\r\n"
                 self.wfile.write(Answer)
-            elif Metodo == "ACK":
-                print "Tratamiento ACK"
-            elif Metodo == "BYE":
-                print "Tratamiento BYE"
             
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python server.py IP port audio_file")
+    if not os.path.exists(sys.argv[3]):
+        sys.exit("El archivo " + sys.argv[3] + " no existe")
     serv = SocketServer.UDPServer(("", int(sys.argv[2])), EchoHandler)
     print "Listening..."
     serv.serve_forever()
