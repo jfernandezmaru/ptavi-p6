5e1c179920a682dface1aa724b22c55b9a75393d
cristian Alvarez perez <calvarez@iota30.aulas.gsyc.es>
2014-11-17 10:47:19 +0100

    Servidor SIP que responde a los INVITE

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
-
+import sys
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
     """
@@ -13,19 +13,27 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
-
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            print "El cliente nos manda " + line
+            Metodo = line.split(" ")[0]
+            if Metodo == "INVITE":
+                Answer = "SIP/2.0 100 Trying\r\n\r\n"
+                Answer += "SIP/2.0 180 Ring\r\n\r\n"
+                Answer += "SIP/2.0 200 OK\r\n\r\n"
+                self.wfile.write(Answer)
+            elif Metodo == "ACK":
+                print "Tratamiento ACK"
+            elif Metodo == "BYE":
+                print "Tratamiento BYE"
+            
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    serv = SocketServer.UDPServer(("", int(sys.argv[2])), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
