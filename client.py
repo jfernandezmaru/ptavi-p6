0fef83f658d02e023188c88ade72da5191e09c36
cristian Alvarez perez <calvarez@iota30.aulas.gsyc.es>
2014-11-17 10:46:48 +0100

    Cliente SIP, con invite y bye, y ack pero sin comprobar los mensajes que llegan

--- client.py
+++ client.py
@@ -5,15 +5,21 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
+Command_Line = sys.argv
+Metodo = Command_Line[1].upper()
+Direccion = Command_Line[2]
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+SERVER = Direccion.split("@")[1].split(":")[0]
+PORT = int(Direccion.split("@")[1].split(":")[1])
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+Metodo = Command_Line[1].upper()
+Direccion = Command_Line[2]
+LINE = Metodo + " sip:" + Direccion.split(":")[0] + " SIP/2.0\r\n\r\n"
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,10 +27,15 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
+my_socket.send(LINE)
 data = my_socket.recv(1024)
-
 print 'Recibido -- ', data
+
+if Metodo != "BYE":
+    LINE = "ACK sip:" + Direccion.split(":")[0] + " SIP/2.0\r\n\r\n"
+    my_socket.send(LINE)
+    
+
 print "Terminando socket..."
 
 # Cerramos todo
