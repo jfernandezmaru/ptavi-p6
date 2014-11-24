bc5e43e84e030b6ad2804505a65be4727a3776ab
javier fernandez marugan <jmarugan@iota22.aulas.gsyc.es>
2014-11-24 17:28:06 +0100

    Practica 6 completa y probada con PEP8

--- client.py
+++ client.py
@@ -4,24 +4,21 @@
 """
 Programa cliente que abre un socket a un servidor
 """
-
 import socket
 import sys
 
 try:
-    """
-    Intentamos construir el Cliente
-    """
+
     METODO = sys.argv[1].upper()
     INICIO = sys.argv[2]
     NICK = INICIO.split("@")[0]
     SERVER = INICIO.split("@")[1].split(":")[0]
     PORT = int(INICIO.split("@")[1].split(":")[1])
-    
+
     if METODO == "INVITE" or METODO == "BYE":
-    
+
         # Contenido que vamos a enviar
-        LINE = METODO + " sip:" + NICK + "@" + SERVER + " SIP/2.0\r\n" 
+        LINE = METODO + " sip:" + NICK + "@" + SERVER + " SIP/2.0\r\n"
         # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -30,18 +27,18 @@ try:
         data = my_socket.recv(1024)
         print "Enviando: " + LINE
         print "Recibido:", data
-        
+
         processed_data = data.split('\r\n\r\n')
 
-        if  processed_data[0] == "SIP/2.0 100 Trying"    and \
-            processed_data[1] == "SIP/2.0 180 Ringing"   and \
-            processed_data[2] == "SIP/2.0 200 OK":
-        
+        if processed_data[0] == "SIP/2.0 100 Trying" and\
+           processed_data[1] == "SIP/2.0 180 Ringing" and\
+           processed_data[2] == "SIP/2.0 200 OK":
+
             LINE = 'ACK' + " sip:" + NICK + "@" + SERVER + " SIP/2.0\r\n"
             my_socket.send(LINE + '\r\n')
-            data = my_socket.recv(1024)        
+            data = my_socket.recv(1024)
             print "Terminando socket..."
-  
+
         # Cerramos todo
         my_socket.close()
         print "Fin."
--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 # Practica 6 Javier Fernandez Marugan  PTAVI
-"""
-Clase (y programa principal) para un servidor de eco en UDP
-"""
 
 import SocketServer
 import sys
@@ -11,56 +8,55 @@ import os
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             if not line:
 
                 self.wfile.write("SIP/2.0 400 Bad Request\r\n")
                 break
             else:
-            
+
                 print "El cliente nos manda " + line
                 check1 = line.find("sip:")
                 check2 = line.find("@")
                 check3 = line.find("SIP")
                 check4 = line.find("/2.0")
                 if check1 >= 0 and check2 >= 0 and check3 >= 0 and check4 >= 0:
+
                     lista = line.split(" ")
                     Metodo = lista[0]
-                    IP_Cliente = str(self.client_address[0])          
+                    IP_Cliente = str(self.client_address[0])
                     if Metodo == "INVITE":
 
-                        self.wfile.write("SIP/2.0 100 Trying\r\n\r\n" + "SIP/"\
-                        + "2.0 180 Ringing\r\n\r\n" + "SIP/2.0 200 OK\r\n\r\n")
+                        message = "SIP/2.0 100 Trying\r\n\r\n"
+                        message = message + "SIP/2.0 180 Ringing\r\n\r\n"
+                        message = message + "SIP/2.0 200 OK\r\n\r\n"
+                        self.wfile.write(message)
+
                     elif Metodo == "ACK":
-                    
+
                         os.system("chmod 777 mp32rtp")
-                        Packet = "./mp32rtp -i " + IP_Cliente + \
-                         " -p 23032 < " + AUDIO
+                        Packet = "./mp32rtp -i " + IP_Cliente + " -p 23032 <"
+                        Packet = Packet + AUDIO
                         os.system(Packet)
                     elif Metodo == "BYE":
-                    
+
                         self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
                         print "Cliente " + IP_Cliente + " cierra la conexión"
                     else:
 
-                        self.wfile.write("SIP/2.0 405"\
-                         + " Method Not Allowed\r\n\r\n")
+                        self.wfile.write("SIP/2.0 405\
+                         Method Not Allowed\r\n\r\n")
                 else:
-                    
+
                     self.wfile.write("SIP/2.0 400 Bad Request\r\n")
             break
 
 if __name__ == "__main__":
 
     try:
-
         SERVER = sys.argv[1]
         PORT = int(sys.argv[2])
         if not os.access(sys.argv[3], os.F_OK):
@@ -69,6 +65,7 @@ if __name__ == "__main__":
         AUDIO = sys.argv[3]
     except ValueError:
         print "Usage: python server.py IP port audio_file"
+
     print "Listening..."
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer((SERVER, PORT), EchoHandler)
